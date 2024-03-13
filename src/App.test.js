c98a6bf8a2c3bce5ca58af1915c6710bd16e9886
Max Kalinich <maksym.kalinich@capgemini.com>
2024-03-13 20:16:22 +0200

    Added failed test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,11 @@ describe('Dummy test suite', () => {
       expect(1).toBe(1);
     });
 
-    it("Dummy test 2 - to be failed", () => {
+    it("Dummy test 2 - to be passed", () => {
         expect(2).toBe(2);
     });
+
+    it("Dummy test 3 - to be failed", () => {
+      expect(3).toBe(4);
+  });
   })
